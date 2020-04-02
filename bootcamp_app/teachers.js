0c766d5a710cc69132cb2d57f6ef73461bc74e9f
Jaspinder <jas@jaspinder.net>
2020-04-02 16:47:02 +0000

    added placeholder values to students and teachers files

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT cohorts.name AS cohort, teachers.name AS teacher
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY cohorts.name, teachers.name
-ORDER BY cohorts.name;
-`)
+ORDER BY $1;
+`;
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} ${user.teacher}`)
